27e94c252d7350f9c6cbddb005e35674b69c49bd
Bryce Hipp <bhipp@qetechnology.com>
2018-10-26 10:50:39 -0500

    Add solution for problem 1

---
+++ js/day-2/problem1.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+const spreadsheet = fs.readFileSync('./input', 'utf8')
+
+function getChecksum() {
+  return spreadsheet
+    .split(/\r\n|\r|\n/g)
+    .map(row => row.split(/\s+/).map(Number))
+    .map(row => row.sort((a, b) => a - b))
+    .map(row => Math.max(...row) - Math.min(...row))
+    .reduce((a, b) => a + b)
+}
+
+console.log(`Checksum: ${getChecksum()}`)
