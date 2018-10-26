3b3cdbadcbfee2f957378bded39cff3b3c25c4b4
Bryce Hipp <bhipp@qetechnology.com>
2018-10-26 11:04:05 -0500

    Remove unneeded sort

--- js/day-2/problem1.js
+++ js/day-2/problem1.js
@@ -6,7 +6,6 @@ function getChecksum() {
   return spreadsheet
     .split(/\r\n|\r|\n/g)
     .map(row => row.split(/\s+/).map(Number))
-    .map(row => row.sort((a, b) => a - b))
     .map(row => Math.max(...row) - Math.min(...row))
     .reduce((a, b) => a + b)
 }
